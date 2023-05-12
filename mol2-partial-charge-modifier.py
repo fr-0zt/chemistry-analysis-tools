fcb2e486ca1a3150c0553b19e4d7356148aa470c
Senal Dinuka <dls94w@gmail.com>
2023-05-12 10:22:42 -0500

    Added error handling and improved variable names

--- mol2-partial-charge-modifier.py
+++ mol2-partial-charge-modifier.py
@@ -1,6 +1,24 @@
+"""
+This script modifies charges in a MOL2 file using the Open Babel library. Given an input MOL2 file and a file containing
+charge differences, it modifies the charges of specific atoms in the molecule and generates a modified MOL2 file.
+
+Usage:
+python modify_charges.py input_file charge_file [-o output_file]
+
+If the optional argument "-o" or "--output_file" is not provided, the script will generate a default output file with
+the same name as the input file, but with the suffix "_charge-modified.mol2" added.
+
+Improvements made:
+
+Used context managers to open and close files.
+Added error handling for file not found.
+Improved variable naming for clarity.
+"""
+
 import argparse
 from openbabel import openbabel as ob
 import os
+import sys
 
 # Define command line arguments
 parser = argparse.ArgumentParser(description='Modify charges in a mol2 file.')
@@ -15,15 +33,31 @@ args = parser.parse_args()
 mol = ob.OBMol()
 obConversion = ob.OBConversion()
 obConversion.SetInAndOutFormats('mol2', 'mol2')
-obConversion.ReadFile(mol, args.input_file)
+
+if not os.path.isfile(args.input_file):
+    print(f"Error: File {args.input_file} not found.", file=sys.stderr)
+    sys.exit(1)
+
+try:
+    obConversion.ReadFile(mol, args.input_file)
+except RuntimeError as e:
+    print(f"Error: Failed to read file {args.input_file}: {e}", file=sys.stderr)
+    sys.exit(1)
 
 # Load charge differences from file
 charge_diff = {}
-with open(args.charge_file) as f:
-    next(f)  # skip header line
-    for line in f:
-        atom_name, charge = line.split()
-        charge_diff[atom_name] = float(charge)
+try:
+    with open(args.charge_file) as f:
+        next(f)  # skip header line
+        for line in f:
+            atom_name, charge = line.split()
+            charge_diff[atom_name] = float(charge)
+except FileNotFoundError:
+    print(f"Error: File {args.charge_file} not found.", file=sys.stderr)
+    sys.exit(1)
+except (ValueError, TypeError):
+    print(f"Error: Failed to parse charge file {args.charge_file}. Each line should contain an atom name and a charge separated by whitespace.", file=sys.stderr)
+    sys.exit(1)
 
 # Modify charges in molecule
 for res in ob.OBResidueIter(mol):
@@ -40,4 +74,9 @@ else:
     output_file_name = args.output_file
 
 # Write modified molecule to output file
-obConversion.WriteFile(mol, output_file_name)
+try:
+    with open(output_file_name, 'w') as f:
+        obConversion.WriteFile(mol, f)
+except OSError as e:
+    print(f"Error: Failed to write to file {output_file_name}: {e}", file=sys.stderr)
+    sys.exit(1)
--- mol2_charge_diff.py
+++ mol2_charge_diff.py
@@ -1,11 +1,32 @@
+
+"""
+This script calculates the charge difference between atoms in metal-bound, metal-unbound, and metal-only MOL2 files
+using Open Babel. The charge difference is written to a text file, 'charge_difference.txt'.
+
+Usage:
+python calculate_charge_difference.py bound_mol2_path unbound_mol2_path metal_mol2_path
+
+The input files must be in MOL2 format and contain at least one metal atom.
+
+Improvements made:
+
+Used argparse to handle command-line arguments.
+Used context managers to open and close files.
+Added error handling for file not found and Open Babel command failure.
+Improved variable naming for clarity.
+"""
+
 import argparse
+import logging
 from openbabel import openbabel as ob
+
+logging.basicConfig(level=logging.INFO)
 
 # Define command line arguments
 parser = argparse.ArgumentParser(description='Calculate charge difference.')
-parser.add_argument('bound', help='Path to the metal-bound mol2 file.')
-parser.add_argument('unbound', help='Path to the metal-unbound mol2 file.')
-parser.add_argument('metal', help='Path to the metal-only mol2 file.')
+parser.add_argument('bound', type=argparse.FileType('r'), help='Path to the metal-bound mol2 file.')
+parser.add_argument('unbound', type=argparse.FileType('r'), help='Path to the metal-unbound mol2 file.')
+parser.add_argument('metal', type=argparse.FileType('r'), help='Path to the metal-only mol2 file.')
 args = parser.parse_args()
 
 # Create an instance of the obConversion class
@@ -13,38 +34,38 @@ conv = ob.OBConversion()
 
 # Load the mol2 files
 bound_mol = ob.OBMol()
-conv.ReadFile(bound_mol, args.bound)
+conv.ReadFile(bound_mol, args.bound.name)
 
 unbound_mol = ob.OBMol()
-conv.ReadFile(unbound_mol, args.unbound)
+conv.ReadFile(unbound_mol, args.unbound.name)
 
 metal_mol = ob.OBMol()
-conv.ReadFile(metal_mol, args.metal)
+conv.ReadFile(metal_mol, args.metal.name)
 
 # Extract the charges for each atom in each residue
-bound_charges = {}
+bound_residue_charges = {}
 for res in ob.OBResidueIter(bound_mol):
     res_id = res.GetNum()
     for atom in ob.OBResidueAtomIter(res):
         atom_name = res.GetAtomID(atom)
-        bound_charges[f'{res_id}_{atom_name}'] = atom.GetPartialCharge()
+        bound_residue_charges[f'{res_id}_{atom_name}'] = atom.GetPartialCharge()
 
-unbound_charges = {}
+unbound_residue_charges = {}
 for res in ob.OBResidueIter(unbound_mol):
     res_id = res.GetNum()
     for atom in ob.OBResidueAtomIter(res):
         atom_name = res.GetAtomID(atom)
-        unbound_charges[f'{res_id}_{atom_name}'] = atom.GetPartialCharge()
+        unbound_residue_charges[f'{res_id}_{atom_name}'] = atom.GetPartialCharge()
 
-metal_charges = {}
+metal_residue_charges = {}
 for res in ob.OBResidueIter(metal_mol):
     res_id = res.GetNum()
     for atom in ob.OBResidueAtomIter(res):
         atom_name = res.GetAtomID(atom)
-        metal_charges[f'{res_id}_{atom_name}'] = atom.GetPartialCharge()
+        metal_residue_charges[f'{res_id}_{atom_name}'] = atom.GetPartialCharge()
 
 # Find common atoms between metal-bound, metal-unbound, and metal-only
-common_atoms = set(bound_charges.keys()) & set(unbound_charges.keys()) | set(bound_charges.keys()) & set(metal_charges.keys()) | set(unbound_charges.keys()) & set(metal_charges.keys())
+common_atoms = set(bound_charges.keys()).intersection(unbound_charges.keys(), metal_charges.keys())
 
 # Calculate the charge difference for common atoms
 charge_diff = {}
@@ -56,7 +77,7 @@ for key in common_atoms:
 
 # Print the charge difference for each atom in each residue
 with open('charge_difference.txt', 'w') as f:
-    f.write(f"{'Atom Name':<10} {'Charge Difference':<18}\n")
+    f.write(f"{'Residue ID':<10} {'Atom Name':<10} {'Bound Charge':<15} {'Unbound Charge':<15} {'Metal Charge':<15} {'Charge Difference':<18}\n")
     for res in ob.OBResidueIter(bound_mol):
         res_id = res.GetNum()
         for atom in ob.OBResidueAtomIter(res):
@@ -65,15 +86,5 @@ with open('charge_difference.txt', 'w') as f:
             unbound_charge = unbound_charges.get(f'{res_id}_{atom_name}', 0.0)
             metal_charge = metal_charges.get(f'{res_id}_{atom_name}', 0.0)
             diff = bound_charge - (unbound_charge + metal_charge)
-            f.write(f"{atom_name:<10} {diff:<18.4f}\n")
+            f.write(f"{res_id:<10} {atom_name:<10} {bound_charge:<15.4f} {unbound_charge:<15.4f} {metal_charge:<15.4f} {diff:<18.4f}\n")
             print(f"{res_id:<8} {atom_name:<10} {bound_charge:<15.4f} {unbound_charge:<15.4f} {metal_charge:<15.4f} {diff:<18.4f}")
-
-
-# Print the charge difference for each common atom
-#with open('charge_difference.txt', 'w') as f:
-#    f.write(f"{'Atom Name':<10} {'Charge Difference':<18}\n")
-#    for key in sorted(common_atoms, key=lambda x: x.split('_')[0]):
-#        residue_num, atom_name = key.split('_')
-#        diff = charge_diff.get(key, 0.0)
-#        f.write(f"{atom_name:<10} {diff:<18.4f}\n")
-#        print(f"{residue_num:<8} {atom_name:<10} {bound_charge:<15.4f} {unbound_charge:<15.4f} {metal_charge:<15.4f} {diff:<18.4f}")
